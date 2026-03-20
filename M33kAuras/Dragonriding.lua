e05dc22400c1117eadf46f7937454e13f0bc7699
m33shoq <mishadyrov@gmail.com>
2026-03-20 23:19:09 +0200

    Use LibAdvFlight for dragonriding trigger

--- M33kAuras/Dragonriding.lua
+++ M33kAuras/Dragonriding.lua
@@ -4,7 +4,8 @@ local AddonName = ...
 ---@class Private
 local Private = select(2, ...)
 
-local GetBonusBarIndex, GetBonusBarOffset = C_ActionBar.GetBonusBarIndex, C_ActionBar.GetBonusBarOffset
+local LibAdvFlight = LibStub("LibAdvFlight-1.1")
+local Events = LibAdvFlight.Events;
 
 local function HandleEvent(self, event, arg1)
   Private.callbacks:Fire("WA_DRAGONRIDING_UPDATE")
@@ -14,14 +15,13 @@ local function HandleEvent(self, event, arg1)
 end
 
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("UNIT_POWER_BAR_SHOW")
-frame:RegisterEvent("UNIT_POWER_BAR_HIDE")
-frame:RegisterEvent('PLAYER_CAN_GLIDE_CHANGED')
-frame:RegisterEvent('PLAYER_IS_GLIDING_CHANGED')
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:SetScript("OnEvent", HandleEvent)
 
+LibAdvFlight.RegisterCallback(Events.ADV_FLYING_ENABLED, HandleEvent);
+LibAdvFlight.RegisterCallback(Events.ADV_FLYING_DISABLED, HandleEvent);
+
 Private.IsDragonriding = function ()
-  return GetBonusBarIndex() == 11 and GetBonusBarOffset() == 5
+  return LibAdvFlight.IsAdvFlyEnabled()
 end
 
