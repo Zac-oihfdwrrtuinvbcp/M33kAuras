1abc0aba94870f6ea9dd68c944610990aaa4c5c1
m33shoq <mishadyrov@gmail.com>
2026-09-19 21:01:51 +0300

    Match Forever weapon type choices to its skill list

--- M33kAuras/Types.lua
+++ M33kAuras/Types.lua
@@ -4532,7 +4532,7 @@ skippedWeaponTypes[11] = true -- Bear Claws
 skippedWeaponTypes[12] = true -- Cat Claws
 skippedWeaponTypes[14] = true -- Misc
 skippedWeaponTypes[17] = true -- Spears
-if M33kAuras.IsClassicOrWrathOrCataOrMists() then
+if M33kAuras.IsClassicOrWrathOrCataOrMists() or M33kAuras.IsForever() then
   skippedWeaponTypes[9] = true -- Glaives
 else
   skippedWeaponTypes[16] = true -- Thrown
