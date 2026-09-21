f8e88efef7f17f24c34b2713b403c208f87e9a0b
m33shoq <mishadyrov@gmail.com>
2026-09-21 07:26:51 +0300

    Avoid idle smooth status bar updates

--- M33kAuras/RegionTypes/SmoothStatusBarMixin.lua
+++ M33kAuras/RegionTypes/SmoothStatusBarMixin.lua
@@ -7,6 +7,9 @@ local Private = select(2, ...)
 -- This is a more or less 1:1 copy of SmoothStatusBarMixin except that it
 -- doesn't clamp the targetValue in ProcessSmoothStatusBars, because that's incorrect for us
 local g_updatingBars = {};
+local smoothingFrame = CreateFrame("Frame")
+Private.frames["Smooth Status Bars"] = smoothingFrame
+smoothingFrame:Hide()
 
 local function IsCloseEnough(bar, newValue, targetValue)
         local min, max = bar:GetMinMaxValues();
@@ -19,6 +22,7 @@ local function IsCloseEnough(bar, newValue, targetValue)
 end
 
 local function ProcessSmoothStatusBars()
+        Private.StartProfileSystem("smooth status bars")
         for bar, targetValue in pairs(g_updatingBars) do
                 local newValue = FrameDeltaLerp(bar:GetValue(), targetValue, .25);
 
@@ -29,9 +33,15 @@ local function ProcessSmoothStatusBars()
                         bar:SetValue(newValue);
                 end
         end
+        if not next(g_updatingBars) then
+                smoothingFrame:Hide()
+        end
+        Private.StopProfileSystem("smooth status bars")
 end
 
-C_Timer.NewTicker(0, ProcessSmoothStatusBars);
+-- Updating once per frame only needs an OnUpdate while a bar is interpolating.
+-- An unconditional zero-delay ticker also fires when there are no auras at all.
+smoothingFrame:SetScript("OnUpdate", ProcessSmoothStatusBars)
 
 Private.SmoothStatusBarMixin = {};
 
@@ -43,10 +53,18 @@ function Private.SmoothStatusBarMixin:ResetSmoothedValue(value) --If nil, tries 
         elseif value then
                 self:SetValue(value);
         end
+        if not next(g_updatingBars) then
+                smoothingFrame:Hide()
+        end
 end
 
 function Private.SmoothStatusBarMixin:SetSmoothedValue(value)
         g_updatingBars[self] = value;
+        if next(g_updatingBars) then
+                smoothingFrame:Show()
+        else
+                smoothingFrame:Hide()
+        end
 end
 
 function Private.SmoothStatusBarMixin:SetMinMaxSmoothedValue(min, max)
